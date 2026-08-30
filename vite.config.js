65b8715197b288d54261b2eca89aa2be51cf2c42
alexwhite2209 <sergwhite2209@gmail.com>
2026-08-30 15:41:49 +0300

    fix vercel asset paths

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/autoservice-site/',
+  base: '/',
   plugins: [react()],
   build: {
     target: 'es2020',
     assetsInlineLimit: 2048,
     rollupOptions: {
       output: {
-        manualChunks: { gsap: ['gsap'] },
+        manualChunks: {
+          gsap: ['gsap'],
+        },
       },
     },
   },
